4a0314a94e5bda6852fcb5a84029282604636334
rmiyoyo <miyoyoraphael@gmail.com>
2023-07-07 22:48:08 +0300

    Add modules to index file and style

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import './style.css';
+import fetchMealData from './modules/api.js';
+import createCard from './modules/card.js';
+// import createModal from './modules/modal.js';
+
+async function fetchAndPopulateCards() {
+  const mealData = await fetchMealData();
+  const cardsContainer = document.getElementById('cardsContainer');
+  let row = document.createElement('div');
+  row.classList.add('row');
+  mealData.forEach((meal, index) => {
+    const card = createCard(meal);
+    row.appendChild(card);
+    if ((index + 1) % 3 === 0) {
+      cardsContainer.appendChild(row);
+      row = document.createElement('div');
+      row.classList.add('row');
+    }
+  });
+  if (row.childElementCount > 0) {
+    cardsContainer.appendChild(row);
+  }
+}
+
+fetchAndPopulateCards();
