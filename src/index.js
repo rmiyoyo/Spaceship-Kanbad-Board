aadf9dc2b7233b0829ab1c1b62118a7d694e6136
rmiyoyo <miyoyoraphael@gmail.com>
2023-07-09 01:01:18 +0300

    Add likes, comments, counter

--- src/index.js
+++ src/index.js
@@ -1,83 +1,60 @@
 import './style.css';
-import createCard from './modules/card.js';
-import createModal from './modules/modal.js';
+import {
+  fetchFoodInfo, enterMeals, oneLike, fetchInfo, showInfo, mealsnumber, showMeals,
+  expandedCard, warningArea, opinionCount, fetchOpinions, fillOpinions, opinionsNumber,
+  opinionQty,
+} from './modules/card.js';
 
-// Step 1: Create a function to send a like to the API
-async function sendLike(item_id) {
-  try {
-    const response = await fetch(
-      'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/msuftdwyyKcBLk/likes/',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          item_id,
-        }),
-      },
-    );
+const foodDescr = await fetchFoodInfo();
+const metricsInfo = await fetchInfo();
+enterMeals(foodDescr);
+const opinionsNo = await mealsnumber();
+showMeals(opinionsNo);
+foodDescr.forEach((element) => {
+  showInfo(metricsInfo, `M${element.idMeal}`);
+});
 
-    if (!response.ok) {
-      throw new Error('Error sending like to API.');
+const mealArea = document.querySelector('.menu');
+mealArea.addEventListener('click', async (e) => {
+  e.preventDefault();
+  if (e.target && e.target.matches('i.heart')) {
+    const uniqueID = e.target.id;
+    await oneLike(uniqueID);
+    const metricsInfo = await fetchInfo();
+    showInfo(metricsInfo, uniqueID);
+  } else if (e.target && e.target.matches('button.comment')) {
+    const uniqueID = e.target.id;
+    const idMeal = uniqueID.replace('opinionFtr', '');
+    expandedCard(foodDescr, idMeal);
+    const metricID = uniqueID.replace('opinionFtr', 'M');
+    const nbComments = await opinionsNumber(metricID);
+    opinionQty(nbComments);
+    const commentData = await fetchOpinions(metricID);
+    if (commentData.length) {
+      fillOpinions(commentData);
     }
-  } catch (error) {
-    console.error('Error sending like:', error);
-  }
-}
-
-export default async function fetchMealData() {
-  try {
-    const response = await fetch(
-      'https://www.themealdb.com/api/json/v1/1/search.php?s=',
-    );
-    if (!response.ok) {
-      throw new Error('Error! Unable to fetch meal data.');
-    }
-    const data = await response.json();
-    return data.meals.slice(0, 9); // Retrieve the first 6 meals from the response
-  } catch (error) {
-    throw new Error(error.message);
-  }
-}
-
-async function fetchAndPopulateCards() {
-  const mealData = await fetchMealData();
-  const cardsContainer = document.getElementById('cardsContainer');
-  let row = document.createElement('div');
-  row.classList.add('row');
-  mealData.forEach((meal, index) => {
-    const card = createCard(meal);
-    row.appendChild(card);
-    if ((index + 1) % 3 === 0) {
-      cardsContainer.appendChild(row);
-      row = document.createElement('div');
-      row.classList.add('row');
-    }
-
-    const modal = createModal(meal);
-    document.body.appendChild(modal);
-    const commentButton = card.querySelector('.btn-comment');
-    commentButton.addEventListener('click', () => {
-      const modalId = `modal${meal.idMeal}`;
-      const modalElement = document.getElementById(modalId);
-      const bootstrapModal = new bootstrap.Modal(modalElement);
-      bootstrapModal.show();
+    const submitCommentBtn = document.querySelector('button.your-opionion');
+    submitCommentBtn.addEventListener('click', async (e) => {
+      e.preventDefault();
+      const userName = document.querySelector('input.user-name').value;
+      const insights = document.querySelector('textarea.insights').value;
+      if (!userName || !insights) {
+        warningArea(userName, insights);
+      }
+      await opinionCount(metricID, userName, insights);
+      document.querySelector('input.user-name').value = '';
+      document.querySelector('textarea.insights').value = '';
+      document.querySelector('div.opinion-scn').innerHTML = '';
+      const commentData = await fetchOpinions(metricID);
+      fillOpinions(commentData);
+      const nbComments = await opinionsNumber(metricID);
+      opinionQty(nbComments);
     });
-
-    // Step 2: Add event listener to the like button
-    const likeButton = card.querySelector('.btn-like');
-    likeButton.addEventListener('click', async () => {
-      await sendLike(meal.idMeal);
-      const likeCountElement = card.querySelector('.like-count');
-      const currentLikeCount = parseInt(likeCountElement.innerText);
-      const newLikeCount = currentLikeCount + 1;
-      likeCountElement.innerText = newLikeCount;
-    });
-  });
-  if (row.childElementCount > 0) {
-    cardsContainer.appendChild(row);
   }
-}
-
-fetchAndPopulateCards();
+});
+const popupWindow = document.querySelector('.popup-window');
+popupWindow.addEventListener('click', (e) => {
+  if (e.target && e.target.matches('i.minimize-card')) {
+    popupWindow.innerHTML = '';
+  }
+});
--- src/modules/card.js
+++ src/modules/card.js
@@ -1,23 +1,187 @@
-export default function createCard(meal) {
-  const card = document.createElement('div');
-  card.classList.add('col-lg-4');
-  const cardContent = `
-    <div class="card" style="background: linear-gradient(to bottom right, #556B2F, #AFEEEE);">
-      <img src="${meal.strMealThumb}" class="card-img-top img-fluid" alt="Food Image">
-      <div class="card-footer d-flex justify-content-between align-items-center">
-        <small class="text-white">${meal.strMeal}</small>
-        <button type="button" class="btn btn-outline-light btn-icon btn-comment">
-          Comment
-          <i class="bi bi-chat-text-fill"></i>
-        </button>
-        <button type="button" class="btn btn-outline-light btn-icon btn-like">
-          <i class="bi bi-heart-fill"></i>
-          Like
-        </button>
-        <span class="like-count">0</span>
-      </div>
-    </div>
-  `;
-  card.innerHTML = cardContent;
-  return card;
-}
+export const fetchFoodInfo = async () => {
+  const request = new Request('https://themealdb.com/api/json/v1/1/search.php?f=c');
+  const response = await fetch(request);
+  const data = await response.json();
+  return data.meals;
+};
+
+export const enterMeals = async (foodDescr) => {
+  const mealArea = document.querySelector('.menu');
+  foodDescr.forEach((element) => {
+    const mealAccrdn = document.createElement('div');
+    mealAccrdn.classList.add('food-card', 'scn-scn');
+    mealAccrdn.innerHTML = `<div class = 'image-box'><img src = '${element.strMealThumb}' alt = 'meal'></div>
+                          <div class = 'ncs-ncs ppup-area'>
+                            <h2 class = 'expand-id'>${element.strMeal}</h2>
+                            <i id ="M${element.idMeal}" class="fa-sharp fa-solid fa-heart heart" style="color: red;"></i><span id ="L${element.idMeal}" class ="likes">0 Likes</span>
+                          </div>
+                          <div class = "press-feature ncs-ncs">
+                            <button id = "opinionFtr${element.idMeal}" class = 'comment'>Comments</button>
+                          </div>
+                          <hr>
+                          `;
+    mealArea.appendChild(mealAccrdn);
+  });
+};
+
+export const createInstance = async () => {
+  const request = new Request('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/');
+  await fetch(request, {
+    method: 'POST',
+  });
+};
+
+export const oneLike = async (uniqueID) => {
+  const request = new Request('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/IJ8iLiH4qdPujPaGR8ab/likes/');
+  await fetch(request, {
+    headers: {
+      'Content-type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: uniqueID,
+    }),
+  });
+};
+
+export const fetchInfo = async () => {
+  const request = new Request('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/IJ8iLiH4qdPujPaGR8ab/likes/');
+  const response = await fetch(request);
+  const data = await response.json();
+  return data;
+};
+
+export const showInfo = async (metricsInfo, metricID) => {
+  metricsInfo.forEach((element) => {
+    if (element.item_id === metricID) {
+      const likeUnique = metricID.replace('M', 'L');
+      const span = document.querySelector(`#${likeUnique}`);
+      span.textContent = `${element.likes} likes`;
+    }
+  });
+};
+
+export const mealsnumber = async () => {
+  const request = new Request('https://themealdb.com/api/json/v1/1/search.php?f=c');
+  const response = await fetch(request);
+  const data = await response.json();
+  return data.meals.length;
+};
+
+export const showMeals = (opinionsNo) => {
+  const li = document.querySelector('li.meals-num');
+  li.textContent = `Meals(${opinionsNo})`;
+};
+
+export const expandedCard = (data, id) => {
+  const expandCard = document.querySelector('.popup-window');
+  data.forEach((element) => {
+    if (element.idMeal === id) {
+      expandCard.innerHTML = `<div class = "card-enclose scn-scn">
+                              <i class="fa-solid fa-xmark minimize-card"></i>
+                              <section class = "food-drn scn-scn">
+                                <div class = 'expand-box scn-scn'>
+                                  <img class = "expand-photo" src = '${element.strMealThumb}' alt = 'meal'>
+                                  <h2 class = "expand-id">${element.strMeal}</h2>
+                                </div>
+                                <div class = "food-dscrptn">
+                                  <div><span>Area:</span> ${element.strArea}</div>
+                                  <div><span>Category:</span> ${element.strCategory}</div>
+                                  <div class = "recipeArea"><span>Instructions:</span> ${element.strInstructions}</div>
+                                </div>
+                              </section>
+                              <hr>
+                              <section class = "opinion-area scn-scn">
+                                <h2 class = "opinion-header">Comments (0)</h2>
+                                <div class = "opinion-scn scn-scn"></div>
+                              </section>
+                              <hr>
+                              <section class = "add-opinion scn-scn">
+                                <h2 class = "enter-opinion">Add a comment</h2>
+                                <form class = "enter-opinion scn-scn">
+                                  <label for = "userName"></label>
+                                  <input id = "userName" class = "user-name" type = "text", required, placeholder = "Your name">
+                                  <p class = "wrong-name"></p>
+                                  <label for = "textArea"></label>
+                                  <textarea id = "textarea" class = "insights" required, placeholder = "Your insights"></textarea>
+                                  <p class = "insights-error"></p>
+                                  <button class = "your-opionion">Comment</button>
+                                </form>
+                              </section>
+                            </div>
+                            `;
+    }
+  });
+};
+
+export const warningArea = (userName, insights) => {
+  const p1 = document.querySelector('p.wrong-name');
+  const p2 = document.querySelector('p.insights-error');
+  if (!userName) {
+    p1.innerHTML = 'required field';
+    p1.style.color = 'orange';
+    p1.style.fontSize = '1rem';
+  } else {
+    p1.innerHTML = '';
+  }
+  if (!insights) {
+    p2.innerHTML = 'required field';
+    p2.style.color = 'orange';
+    p2.style.fontSize = '1rem';
+  } else {
+    p2.innerHTML = '';
+  }
+};
+
+export const opinionCount = async (id, userName, insights) => {
+  const request = new Request('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/IJ8iLiH4qdPujPaGR8ab/comments');
+  await fetch(request, {
+    headers: {
+      'Content-type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: id,
+      username: userName,
+      comment: insights,
+    }),
+  });
+};
+
+export const fetchOpinions = async (id) => {
+  const request = new Request(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/IJ8iLiH4qdPujPaGR8ab/comments?item_id=${id}`);
+  const response = await fetch(request);
+  const data = await response.json();
+  return data;
+};
+
+export const fillOpinions = (data) => {
+  const opinionFld = document.querySelector('div.opinion-scn');
+  data.forEach((element) => {
+    if (element.creation_date && element.username && element.comment) {
+      const p = document.createElement('p');
+      p.textContent = `${element.creation_date} ${element.username}: ${element.comment}`;
+      opinionFld.appendChild(p);
+    }
+  });
+};
+
+export const opinionsNumber = async (id) => {
+  const request = new Request(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/IJ8iLiH4qdPujPaGR8ab/comments?item_id=${id}`);
+  const response = await fetch(request);
+  const data = await response.json();
+  let a = 0;
+  if (data.length) {
+    data.forEach((element) => {
+      if (element.creation_date && element.username && element.comment) {
+        a += 1;
+      }
+    });
+  }
+  return a;
+};
+
+export const opinionQty = (opinionsNo) => {
+  const h2 = document.querySelector('h2.opinion-header');
+  h2.textContent = `Number of Comments (${opinionsNo})`;
+};
